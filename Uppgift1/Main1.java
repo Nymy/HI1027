bb4d40b39cbbc4ee183b633548412a3d2f394bf5
Nymy <79588945+Nymy@users.noreply.github.com>
2021-08-23 10:40:56 +0200

    Create Main1.java

---
+++ Uppgift1/Main1.java
@@ -0,0 +1,9 @@
+// Create a dice that shows the upside 1 when first created
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        Dice dice = new Dice(6);
+        System.out.println(dice.toString());
+    }
+}
